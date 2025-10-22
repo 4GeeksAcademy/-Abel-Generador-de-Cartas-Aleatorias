19850f17f083d5be8a47e1c4234e3efb4588b9cc
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2025-10-22 19:58:07 +0000

    estructura inicial para generar cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function randomNumberOfArray(array) {
+	return Math.floor(Math.random() * array.length)
+}
+
+window.onload = function () {
+	//write your code here
+	GenerateRandomCard();
 };
+
+function GenerateRandomCard() {
+	let suits = ["♠", "♥",  "♣", "♦"];
+	let ranks = ["A", 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+	let rank = randomNumberOfArray(ranks);
+	let suit = randomNumberOfArray(suits);
+	let text = document.querySelector("body")
+	if (suit %2 === 0) text.style.color = "black";
+	else text.style.color = "red";
+	document.querySelector("span").innerHTML = ranks[rank];
+	document.querySelector("header").innerHTML = suits[suit];
+	document.querySelector("footer").innerHTML = suits[suit];
+}
