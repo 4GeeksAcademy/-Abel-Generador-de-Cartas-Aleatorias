e0f0cdaa6e3f78978b67ee52be9ef86a36a154f6
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2025-10-23 14:37:12 +0000

    feat: mejorar la funcionalidad de redimensionamiento de cartas y ajustar estilos

--- src/app.js
+++ src/app.js
@@ -28,22 +28,28 @@ const GenerateRandomCard = () => {
 }
 
 const resizingCards = () => {
+	console.log("resizingCards");
 	const widthCard = document.querySelector("#widthCard").value;
 	const heightCard = document.querySelector("#heightCard").value;
 	const cards = document.querySelectorAll(".card");
-	console.log(widthCard, heightCard, cards);
 	cards.forEach(card => {
 		card.style.width = widthCard + "px";
+		card.style.fontSize = (card.style.width) ? 
+							`${widthCard * 40/100}px` : 
+							`${card.computedStyleMap().get("width").value *40/100 }px`;
 		card.style.height = heightCard + "px";
 	});
 }
 
 
-
 window.onload = function () {
 	//write your code here
 	GenerateRandomCard();
-	document.querySelector("#createNewCard").addEventListener("click", GenerateRandomCard);
+	resizingCards();
+	document.querySelector("#createNewCard").addEventListener("click", () => {
+		GenerateRandomCard();
+		resizingCards();
+	});
 	// setInterval(GenerateRandomCard, 10000);
 	document.querySelectorAll("#widthCard, #heightCard").forEach(input => {
 		input.addEventListener("input", resizingCards);
